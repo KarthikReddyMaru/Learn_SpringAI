7658a7e1f611462930f3d9dae6ff36d8b4ad4d1f
Maru Karthik Reddy <karthikreddy.maru@gmail.com>
2026-06-14 17:32:05 +0530

    feat: add memory to conversation

--- src/main/java/com/learn/controller/ChatController.java
+++ src/main/java/com/learn/controller/ChatController.java
@@ -1,20 +1,38 @@
 package com.learn.controller;
 
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-@RequiredArgsConstructor
 public class ChatController {
 
-    private final OllamaChatModel ollamaChatModel;
+    private final ChatClient ollamaChatClient;
+
+    public ChatController(OllamaChatModel ollamaChatModel, ChatMemory chatMemory) {
+        this.ollamaChatClient = ChatClient.builder(ollamaChatModel)
+                                          .defaultAdvisors(MessageChatMemoryAdvisor
+                                                                   .builder(chatMemory)
+                                                                   .build())
+                                          .build();
+    }
 
     @GetMapping("/ask")
-    public ResponseEntity<String> ask(String query) {
-        String response = ollamaChatModel.call(query);
+    public ResponseEntity<String> ask(@RequestParam String query) {
+        log.info("Prompt: {}", query);
+        ChatClient.CallResponseSpec responseSpec = ollamaChatClient
+                .prompt(query)
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, "123"))
+                .call();
+
+        String response = responseSpec.content();
         return ResponseEntity.ok(response);
     }
 
